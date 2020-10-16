c1954beda25369d451ae3f70bcb56adc10bc00c9
Devin Zhou <devinzhou101@gmail.com>
2020-10-15 21:13:27 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
+    <div class="container">
+      <header>
+        <a href="/">React Shopping List</a>
       </header>
+      <main>Product List</main>
+      <footer>Footer</footer>
     </div>
   );
 }
