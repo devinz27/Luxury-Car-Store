5b535c3d38d680c03a33920543ce2888f7eca731
Devin Zhou <devinzhou101@gmail.com>
2020-10-17 01:16:46 -0400

    Updates

---
+++ precache-manifest.dacbf38ee6f22450c03e19e1ad2a8e0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b97381077b83a6a2031a0db23b74d95d",
+    "url": "/Luxury-Car-Store/index.html"
+  },
+  {
+    "revision": "b57cbaf6ad695c50adc4",
+    "url": "/Luxury-Car-Store/static/css/main.1d3c3f58.chunk.css"
+  },
+  {
+    "revision": "08afd76624a1e33e0cad",
+    "url": "/Luxury-Car-Store/static/js/2.907f0f95.chunk.js"
+  },
+  {
+    "revision": "13e061b2fd50c1d7f7b7e0bb533040d6",
+    "url": "/Luxury-Car-Store/static/js/2.907f0f95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b57cbaf6ad695c50adc4",
+    "url": "/Luxury-Car-Store/static/js/main.c10cc0fc.chunk.js"
+  },
+  {
+    "revision": "803984d7b2babc133fa1",
+    "url": "/Luxury-Car-Store/static/js/runtime-main.9ac9bcd1.js"
+  }
+]);
